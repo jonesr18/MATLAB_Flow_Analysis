cb0ed616999fe881b03655056e7b0f7eefb3a8ca
Ross Jones <jonesr18@gmail.com>
2020-02-01 19:02:22 -0500

    Added nanflag option to geostd
    Similar to the option in most builtin summary statistic functions.

--- functions/geostd.m
+++ functions/geostd.m
@@ -1,22 +1,24 @@
-function gsd = geostd(x, flag, dim)
+function gsd = geostd(x, flag, dim, nanflag)
 % For vectors, GEOSTD(X) calculates the geometric standard deviation of
 % input X.  For  matrices GEOSTD calculates the geometric standard
 % deviation over each column of X.  For N-D arrays, GEOSTD calculates the
 % geometric standard deviation over the first non-singleton dimension of X.
 % 
 % 
-% WARNING: By default GEOSTD normalises by N, where N is the sample size.
-% This is different to the way the built-in MATLAB function STD operates.
-% 
-% 
-% GEOSTD(X, 0) calculates the geometric standard deviation as above, with
-% normalisation factor (N-1).  GEOSTD(X, 1) works like GEOSTD(X).
-% 
+% GEOSTD(X,1) normalizes by N and produces the square root of the second
+% moment of the sample about its mean. GEOSTD(X,0) is the same as GEOSTD(X).
 % 
 % GEOSTD(X, [], DIM) calculates the geometric standard deviation along
 % dimension DIM of X.  Use a flag of 0 to normalise by factor (N-1), or a
 % flag of 1 to normalise by factor N.
 % 
+% GEOSTD(X, [], DIM, NANFLAG) specifies how NaN (Not-A-Number) values are treated.
+% The default is 'includenan':
+% 
+% 'includenan' - the standard deviation of a vector containing NaN 
+%                values is also NaN.
+% 'omitnan'    - elements of X or W containing NaN values are ignored.
+%                If all elements are NaN, the result is NaN.
 % 
 % NOTE: Class type checking and error handling are conducted within EXP,
 % STD and LOG.
@@ -41,6 +43,9 @@ function gsd = geostd(x, flag, dim)
 % Update Log:
 %	2017-09-28		Ross Jones
 %		Empty input now returns NaN output
+%	2019-05-03		Ross Jones
+%		Added option to omit NaNs from analysis
+%		Changed default behavior to 'normal' for std normalization
 
 
 % Basic error checking of inputs
@@ -58,7 +63,7 @@ end
 
 % Setup default flag where required
 if nargin < 2 || isempty(flag)
-    flag = 1;
+    flag = 0;
 end
 
 % If dimension is not specified, find first non-singleton dimension
@@ -69,13 +74,18 @@ if nargin < 3 || isempty(dim)
     end
 end
 
+% If nanflag is not specified, include NaNs
+if nargin < 4 || isempty(nanflag)
+	nanflag = 'includenan';
+end
+
 % Turn off warnings regarding log of zero, since this is an artifact of the
 % technique use to calculate the gsd
 lozwarning = warning('off', 'MATLAB:log:logOfZero');
 
 % Calculate geometric std dev using 
 % "log of geometric std dev of data = arithmetic std dev of log of data"
-gsd = exp(std(log(x), flag, dim));
+gsd = exp(std(log(x), flag, dim, nanflag));
 
 % Reset warning value
 warning(lozwarning);
